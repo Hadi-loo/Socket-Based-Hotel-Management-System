d6bb9b1755162a0638db17bd02ac7113c8a2d226
Alireza-Zwolf <alireza2621@gmail.com>
2023-03-07 19:08:09 +0330

    Add errors.hpp based on #9

---
+++ src/errors.hpp
@@ -0,0 +1,25 @@
+#include<string>
+
+
+
+const std::string er101 = "Err -> 101: The desired room was not found.\n";
+const std::string er102 = "Err -> 102: Your reservation wans not found.\n";
+const std::string er104 = "Err -> 104: Successfully added.\n";
+const std::string er105 = "Err -> 105: Successfully modified.\n";
+const std::string er106 = "Err -> 106: Successfully deleted.\n";
+const std::string er108 = "Err -> 108: Your account balance is not enogh.\n";
+const std::string er109 = "Err -> 109: The room capacity is full.\n";
+const std::string er110 = "Err -> 110: Successfully done.\n";
+const std::string er111 = "Err -> 111: This room already exists.\n";
+const std::string er201 = "Err -> 201: User logged out successfully.\n";
+const std::string er230 = "Err -> 230: User logged in.\n";
+const std::string er231 = "Err -> 231: User succesfully signed up.\n";
+const std::string er311 = "Err -> 311: User signed up. Enter your password, purse, phone and address.\n";
+const std::string er312 = "Err -> 312: Information was changed successfully.\n";
+const std::string er401 = "Err -> 401: Invalid value!\n";
+const std::string er403 = "Err -> 403: Access denied!\n";
+const std::string er412 = "Err -> 412: Invalid capacity value!\n";
+const std::string er413 = "Err -> 413: Successfully leaving.\n";
+const std::string er430 = "Err -> 430: Invalid username or password.\n";
+const std::string er451 = "Err -> 451: User already existed!\n";
+const std::string er503 = "Err -> 503: Bad sequence of commands.\n";
