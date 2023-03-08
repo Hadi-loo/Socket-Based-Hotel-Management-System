9af0acfbd9b708de02c494eea27a8310e7b6d890
Hadi-loo <m.h.babalu@gmail.com>
2023-03-08 17:54:32 +0330

    Added .json files names to defs.hpp

--- src/defs.hpp
+++ src/defs.hpp
@@ -3,6 +3,9 @@
 
 #define MAX_BUFFER_SIZE 1024
 #define CONFIGS_PATH "configuration"
+#define CONFIG_FILE_NAME "config.json"
+#define ROOMSINFO_FILE_NAME "RoomsInfo.json"
+#define USERSINFO_FILE_NAME "UsersInfo.json"
 
 
 #endif
