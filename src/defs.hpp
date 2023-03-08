88461ce37b56aed79d064c71072e02f700c8e850
Hadi-loo <m.h.babalu@gmail.com>
2023-03-08 20:33:56 +0330

    Cleaned defs.hpp file

--- src/defs.hpp
+++ src/defs.hpp
@@ -1,13 +1,13 @@
 #ifndef _DEFS_HPP_
 #define _DEFS_HPP_
 
-#define MAX_BUFFER_SIZE 1024
-#define CONFIGS_PATH "configuration"
-#define CONFIG_FILE_NAME "config.json"
-#define ROOMSINFO_FILE_NAME "RoomsInfo.json"
-#define USERSINFO_FILE_NAME "UsersInfo.json"
-#define LOGS_PATH "logs"
-#define SERVER_LOG_FILE_NAME "server.log"
+#define MAX_BUFFER_SIZE                     1024
+#define CONFIGS_PATH                        "configuration"
+#define CONFIG_FILE_NAME                    "config.json"
+#define ROOMSINFO_FILE_NAME                 "RoomsInfo.json"
+#define USERSINFO_FILE_NAME                 "UsersInfo.json"
+#define LOGS_PATH                           "logs"
+#define SERVER_LOG_FILE_NAME                "server.log"
 
 
 #endif
