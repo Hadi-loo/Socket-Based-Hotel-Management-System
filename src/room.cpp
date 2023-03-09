2c5068ab72831de6554e9ba147b04b55f0588360
Alireza-Zwolf <alireza2621@gmail.com>
2023-03-09 23:34:30 +0330

    Add show_info function to the Room class

--- src/room.cpp
+++ src/room.cpp
@@ -1,4 +1,7 @@
 #include "room.hpp"
+#include "iostream"
+
+using namespace std;
 
 Room::Room(int _id, int _max_capacity, int _available_capacity, int _price, bool _is_available, vector<Reservation*> _reservations) {
     id = _id;
@@ -12,3 +15,17 @@ Room::Room(int _id, int _max_capacity, int _available_capacity, int _price, bool
 vector<Reservation*> Room::get_reservations(){
     return reservations;
 }
+
+void Room::show_info(){
+    cout << "ID: " << id << endl;
+    cout << "Max Capacity: " << max_capacity << endl;
+    cout << "Available Capacity: " << available_capacity << endl;
+    cout << "Price: " << price << endl;
+    cout << "Is Available: " << (is_available ? "Yes" : "No") << endl;
+    cout << "Reservations: " << endl;
+    for (int i = 0; i < reservations.size(); i++) {
+        cout << "Reservation " << i + 1 << ":" << endl;
+        reservations[i]->show_info();
+    }
+}
+
--- src/room.hpp
+++ src/room.hpp
@@ -18,6 +18,7 @@ private:
 
 public:
     Room(int id, int max_capacity, int available_capacity, int price, bool is_available, vector<Reservation*> reservations);
+    void show_info();
     vector<Reservation*> get_reservations();
 };
 
