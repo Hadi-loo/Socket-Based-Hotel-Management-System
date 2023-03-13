7c7ee30f1f6d372afb66d465606abc78556faf62
Hadi-loo <m.h.babalu@gmail.com>
2023-03-13 18:15:38 +0330

    Cleaned client files based on #5

--- src/client.cpp
+++ src/client.cpp
@@ -51,145 +51,16 @@ void authentication_menu(int server_fd, bool &logged_in, Parser &client_parser) 
         }
 
         else if (input[0] == "signup") {
-            
-            // check if user is already logged in
-            if (logged_in) {
-                // TODO: print error message
-                pretty_write("You are already logged in\n", "red");
+            bool should_continue = handle_signup(logged_in, request, response, server_fd, buff, input);
+            if (should_continue) {
                 continue;
-            }
-            
-            // check if user entered username
-            if (input.size() != 2) {
-                // TODO: print error message
-                cout << RED;
-                cout << "Invalid arguments count: " << input.size() << "\n";
-                cout << RESET;
-                continue;
-            }
-            
-            request["command"] = "signup";
-            request["username"] = input[1];
-                        
-            send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
-            memset(buff, 0, MAX_BUFFER_SIZE);
-            read(server_fd, buff, MAX_BUFFER_SIZE);
-            response = nlohmann::json::parse(buff);
-            
-            // check if username was valid or not
-            if (response["status"] == 451) {
-                // CODE 451: username already exists
-                // TODO: print error message
-                cout << RED;
-                cout << "Username already exists\n";
-                cout << RESET;
-                continue;
-            }
-            
-            else if (response["status"] == 311) {
-                string password, balance, phone_number, address;
-                cout << GREEN;
-                cout << "User Created Successfully. ";
-                cout << RESET;
-                cout << "Enter the following information:\n";
-                pretty_write(">> Password: ", "cyan");
-                cin >> password;
-                pretty_write(">> Balance: ", "cyan");
-                cin >> balance;
-                pretty_write(">> Phone Number: ", "cyan");
-                cin >> phone_number;
-                pretty_write(">> Address: ", "cyan");
-                cin >> address;
-
-                request["command"] = "signup_info";
-                request["password"] = password;
-                request["balance"] = balance;
-                request["phone_number"] = phone_number;
-                request["address"] = address;
-
-                send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
-                memset(buff, 0, MAX_BUFFER_SIZE);
-                read(server_fd, buff, MAX_BUFFER_SIZE);
-                response = nlohmann::json::parse(buff);
-
-                // check if signup was successful
-                if (response["status"] == 503) {
-                    // CODE 503: invalid arguments
-                    // TODO: print error message
-                    cout << RED;
-                    cout << "Invalid arguments: ";
-                    cout << RESET;
-                    cout << response["message"] << "\n";
-                    continue;
-                }
-                else if (response["status"] == 430) {
-                    // CODE 430: user not found
-                    // TODO: print error message
-                    cout << RED;
-                    cout << "User not found!\n";
-                    cout << RESET;
-                    continue;
-                }
-                else if (response["status"] == 231) {
-                    // CODE 231: user info changed successfully
-                    // TODO: print success message
-                    cout << GREEN;
-                    cout << "Successfully signed up!\n";
-                    cout << RESET;
-                    continue;
-                }
             }
         }
 
         else if (input[0] == "signin") {
-                
-            // check if user is already logged in
-            if (logged_in) {
-                // TODO: print error message
-                cout << RED;
-                cout << "You are already logged in\n";
-                cout << RESET;
+            bool should_continue = handle_signin(logged_in, request, response, server_fd, buff, input, client_parser);
+            if (should_continue) {
                 continue;
-            }
-        
-            // check if user entered username
-            if (input.size() != 3) {
-                // TODO: print error message
-                cout << RED;
-                cout << "Invalid arguments count\n";
-                cout << RESET;
-                continue;
-            }
-
-            request["command"] = "signin";
-            request["username"] = input[1];
-            request["password"] = input[2];
-
-            send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
-            memset(buff, 0, MAX_BUFFER_SIZE);
-            read(server_fd, buff, MAX_BUFFER_SIZE);
-            response = nlohmann::json::parse(buff);
-
-            // check if singin was successful or not
-            if (response["status"] == 430) {
-                // CODE 430: 
-                // TODO: print error message
-                cout << RED;
-                cout << "can't signin: ";
-                cout << RESET;
-                cout << response["message"] << "\n";
-                continue;
-            }   
-
-            // login successful
-            else if (response["status"] == 230) {
-                // CODE 230: login successful
-                // TODO: print success message
-                logged_in = true;
-                cout << GREEN;
-                cout << "Successfully signed in!\n";
-                cout << RESET;
-                main_menu(server_fd, logged_in, client_parser);
             }
         }
 
@@ -259,6 +130,150 @@ void pretty_write(string message, string color) {
 
     write(1, message.c_str(), message.size());
     write(1, "\033[0m", 5);
+}
+
+bool handle_signup(bool &logged_in, nlohmann::json &request, nlohmann::json &response, int server_fd, char *buff, vector<string> &input) {
+    // check if user is already logged in
+    if (logged_in) {
+        // TODO: print error message
+        pretty_write("You are already logged in\n", "red");
+        return true;
+    }
+    
+    // check if user entered username
+    if (input.size() != 2) {
+        // TODO: print error message
+        cout << RED;
+        cout << "Invalid arguments count: " << input.size() << "\n";
+        cout << RESET;
+        return true;
+    }
+    
+    request["command"] = "signup";
+    request["username"] = input[1];
+                
+    send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
+    memset(buff, 0, MAX_BUFFER_SIZE);
+    read(server_fd, buff, MAX_BUFFER_SIZE);
+    response = nlohmann::json::parse(buff);
+    
+    // check if username was valid or not
+    if (response["status"] == 451) {
+        // CODE 451: username already exists
+        // TODO: print error message
+        cout << RED;
+        cout << "Username already exists\n";
+        cout << RESET;
+        return true;
+    }
+    
+    else if (response["status"] == 311) {
+        string password, balance, phone_number, address;
+        cout << GREEN;
+        cout << "User Created Successfully. ";
+        cout << RESET;
+        cout << "Enter the following information:\n";
+        pretty_write(">> Password: ", "cyan");
+        cin >> password;
+        pretty_write(">> Balance: ", "cyan");
+        cin >> balance;
+        pretty_write(">> Phone Number: ", "cyan");
+        cin >> phone_number;
+        pretty_write(">> Address: ", "cyan");
+        cin >> address;
+
+        request["command"] = "signup_info";
+        request["password"] = password;
+        request["balance"] = balance;
+        request["phone_number"] = phone_number;
+        request["address"] = address;
+
+        send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
+        memset(buff, 0, MAX_BUFFER_SIZE);
+        read(server_fd, buff, MAX_BUFFER_SIZE);
+        response = nlohmann::json::parse(buff);
+
+        // check if signup was successful
+        if (response["status"] == 503) {
+            // CODE 503: invalid arguments
+            // TODO: print error message
+            cout << RED;
+            cout << "Invalid arguments: ";
+            cout << RESET;
+            cout << response["message"] << "\n";
+            return true;
+        }
+        else if (response["status"] == 430) {
+            // CODE 430: user not found
+            // TODO: print error message
+            cout << RED;
+            cout << "User not found!\n";
+            cout << RESET;
+            return true;
+        }
+        else if (response["status"] == 231) {
+            // CODE 231: user info changed successfully
+            // TODO: print success message
+            cout << GREEN;
+            cout << "Successfully signed up!\n";
+            cout << RESET;
+            return true;
+        }
+    }
+    return true;
+}
+
+bool handle_signin(bool &logged_in, nlohmann::json &request, nlohmann::json &response, int server_fd, char *buff, vector<string> &input, Parser &client_parser) {
+    // check if user is already logged in
+    if (logged_in) {
+        // TODO: print error message
+        cout << RED;
+        cout << "You are already logged in\n";
+        cout << RESET;
+        return true;
+    }
+
+    // check if user entered username
+    if (input.size() != 3) {
+        // TODO: print error message
+        cout << RED;
+        cout << "Invalid arguments count\n";
+        cout << RESET;
+        return true;
+    }
+
+    request["command"] = "signin";
+    request["username"] = input[1];
+    request["password"] = input[2];
+
+    send(server_fd, request.dump().c_str(), strlen(request.dump().c_str()), 0);
+    memset(buff, 0, MAX_BUFFER_SIZE);
+    read(server_fd, buff, MAX_BUFFER_SIZE);
+    response = nlohmann::json::parse(buff);
+
+    // check if singin was successful or not
+    if (response["status"] == 430) {
+        // CODE 430: 
+        // TODO: print error message
+        cout << RED;
+        cout << "can't signin: ";
+        cout << RESET;
+        cout << response["message"] << "\n";
+        return true;
+    }   
+
+    // login successful
+    else if (response["status"] == 230) {
+        // CODE 230: login successful
+        // TODO: print success message
+        logged_in = true;
+        cout << GREEN;
+        cout << "Successfully signed in!\n";
+        cout << RESET;
+        main_menu(server_fd, logged_in, client_parser);
+    }
+
+    return true;
 }
 
 
--- src/client.hpp
+++ src/client.hpp
@@ -23,5 +23,8 @@ void authentication_menu(int server_fd, bool &logged_in, Parser &client_parser);
 void main_menu(int server_fd, bool &logged_in, Parser &client_parser);
 void show_main_menu();
 void pretty_write(string message, string color);
+bool handle_signup(bool &logged_in, nlohmann::json &request, nlohmann::json &response, int server_fd, char *buff, vector<string> &input);
+bool handle_signin(bool &logged_in, nlohmann::json &request, nlohmann::json &response, int server_fd, char *buff, vector<string> &input, Parser &client_parser);
+
 
 #endif
