787517178219d7aecc8d288be531d7ac1f455706
Alireza-Zwolf <alireza2621@gmail.com>
2023-03-14 23:15:57 +0330

    Resovle a conflict

--- src/room.hpp
+++ src/room.hpp
@@ -19,12 +19,11 @@ private:
 public:
     Room(int id, int max_capacity, int available_capacity, int price, bool is_available, vector<Reservation*> reservations);
 
-    int get_id();
     vector<Reservation*> get_reservations();
 
     void show_info();
     string get_info();
-    int get_id(){return id;}
+    int get_id();
     int get_max_capacity(){return max_capacity;}
     int get_price(){return price;}
     void add_reservation(Reservation* reservation);
